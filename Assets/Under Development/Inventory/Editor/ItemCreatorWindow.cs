8f55fc6a2cd8005dc8296f349dbe38fc2f599ca9
Jacob L. Olsen <jacoblynggaardolsen@hotmail.com>
2017-07-03 13:00:55 +0200

    moved files

---
+++ Assets/Under Development/Inventory/Editor/ItemCreatorWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ItemCreatorWindow : EditorWindow {
+
+	[MenuItem ("Tools/Item Creator")]
+	public static void ShowWindow(){
+		EditorWindow.GetWindow (typeof(ItemCreatorWindow));
+	}
+}
