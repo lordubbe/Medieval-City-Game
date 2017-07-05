0be30604b9997e46cba0e8c1b2e9fa4eb7415f2b
Bjeck <bjarke@larsen-dk.dk>
2017-07-05 17:31:17 +0200

    some initial story setup

---
+++ Assets/Scripts/Story/Story.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Story : MonoBehaviour {
+
+	public string storyname;
+	public bool isActive = false;
+	public List<Flowchart> flowcharts = new List<Flowchart>();
+	public Dictionary<string, bool> storyflags = new Dictionary<string,bool>();
+	public List<StoryState> states = new List<StoryState>();
+	StoryState curState;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void ChangeState(StoryState s){
+		curState.OnExit();
+
+		curState = s;
+
+		curState.OnCall();
+	}
+
+
+
+
+}
+
