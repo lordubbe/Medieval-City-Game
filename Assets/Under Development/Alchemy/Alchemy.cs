f1ae2c8483b6d2804e5b5d74363d92f9eb6c1d48
Bjeck <bjarke@larsen-dk.dk>
2017-07-06 13:13:39 +0200

    alchemy setup

---
+++ Assets/Under Development/Alchemy/Alchemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Element { Force, Secrets, Beauty, Sin, Change }
+
+public class Alchemy : MonoBehaviour {
+
+    public Dictionary<Element, int> elements = new Dictionary<Element, int>
+        {
+            { Element.Beauty, 0 },
+            { Element.Sin, 0 },
+            { Element.Change, 0 },
+            { Element.Force, 0 },
+            { Element.Secrets, 0 }
+        };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Under Development/Alchemy/AlchemyProblem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AlchemyProblem : MonoBehaviour {
+
+
+    //ORDER: SIN, CHANGE, FORCE, SECRETS, BEAUTY
+    [SerializeField]
+    List<int> elementValues = new List<int>();
+    
+    public Dictionary<Element, int> unbalancedElements = new Dictionary<Element, int>
+        {
+            { Element.Beauty, 0 },
+            { Element.Sin, 0 },
+            { Element.Change, 0 },
+            { Element.Force, 0 },
+            { Element.Secrets, 0 }
+        };
+
+    // Use this for initialization
+    virtual public void Start () {
+        SetElementTargets();
+
+    }
+
+
+    void SetElementTargets()
+    {
+        unbalancedElements[Element.Sin] = elementValues[0];
+        unbalancedElements[Element.Change] = elementValues[1];
+        unbalancedElements[Element.Force] = elementValues[2];
+        unbalancedElements[Element.Secrets] = elementValues[3];
+        unbalancedElements[Element.Beauty] = elementValues[4];
+
+    }
+
+    public bool CheckSuccess() {
+        foreach(Element e in unbalancedElements.Keys)
+        {
+            if(unbalancedElements[e] < 10 && unbalancedElements[e] > -10)
+            {
+                print("SUCESS");
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+}
