98edb25453401ccad37515a12f7c582b6e7265ec
Bjeck <bjarke@larsen-dk.dk>
2017-07-06 17:10:09 +0200

    WAIT system and serialize

--- Assets/Under Development/Alchemy/AlchemyUI.cs
+++ Assets/Under Development/Alchemy/AlchemyUI.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class AlchemyUI : MonoBehaviour {
 
+    [SerializeField]
     List<Image> alcImages = new List<Image>();
 
 	// Use this for initialization
