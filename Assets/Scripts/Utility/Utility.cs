36e43b085ed175b78dfb3eca9b9737785270678c
Bjeck <bjarke@larsen-dk.dk>
2017-10-06 21:41:55 +0200

    utility from call me

---
+++ Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,373 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A static utility class for my lerps. Will probably move more things in here, once I clean up all the lerping coroutines.
+/// </summary>
+public static class Utility {
+
+    /// <summary>
+    /// These lerps function like regular Mathf.Lerp, except they don't clamp animation at 0-1, so I can overshoot and undershoot with animationCurves. Value must be a number between 0 and 1.
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <param name="value"></param>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    public static float UnClampedAnimationLerp(float start, float end, float value, AnimationCurve c)
+    {
+        return start + ((end - start) * c.Evaluate(value));
+    }
+
+
+    public static Vector3 UnClampedAnimationLerp(Vector3 start, Vector3 end, float value, AnimationCurve c)
+    {
+        return start + ((end - start) * c.Evaluate(value));
+    }
+
+    public static Vector2 UnClampedAnimationLerp(Vector2 start, Vector2 end, float value, AnimationCurve c)
+    {
+        return start + ((end - start) * c.Evaluate(value));
+    }
+
+
+    public static IEnumerator LerpFill(float old, float newnr, Image val, AnimationCurve c, float speed)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            val.fillAmount = UnClampedAnimationLerp(old, newnr, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public static IEnumerator LerpNr(float old, float newnr, float val, AnimationCurve c, float speed)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            val = UnClampedAnimationLerp(old, newnr, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General coroutine for scaling a recttransform, from old to new, with animation curve.
+    /// </summary>
+    public static IEnumerator Scale(Vector3 oldS, Vector3 newS, RectTransform rt, AnimationCurve c, float speed)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(oldS, newS, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General coroutine for scaling a recttransform, from old to new, with animation curve, randomization, delay and speed possible
+    /// </summary>
+    public static IEnumerator Scale(Vector3 oldS, Vector3 newS, RectTransform rt, AnimationCurve c, float speed, float delay)
+    {
+
+        if (delay < 0)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(oldS, newS, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General coroutine for scaling a recttransform, from old to new, with animation curve, randomization, delay and speed possible
+    /// </summary>
+    public static IEnumerator Scale(Vector3 oldS, Vector3 newS, RectTransform rt, AnimationCurve c, float speed, float delay, float random)
+    {
+
+        if (delay < 0)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed + Random.Range(-random,random);
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(oldS, newS, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// General coroutine for scaling a Recttransform up and down.
+    /// </summary>
+    /// <param name="oldS"></param>
+    /// <param name="newS"></param>
+    /// <param name="rt"></param>
+    /// <param name="c"></param>
+    /// <param name="c2"></param>
+    /// <param name="speed"></param>
+    /// <returns></returns>
+    public static IEnumerator ScaleUpAndDown(Vector3 oldS, Vector3 newS, RectTransform rt, AnimationCurve c, AnimationCurve c2, float speed)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(oldS, newS, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+
+        t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(newS, oldS, t, c2);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General coroutine for scaling a Recttransform up and down.
+    /// </summary>
+    /// <param name="oldS"></param>
+    /// <param name="newS"></param>
+    /// <param name="rt"></param>
+    /// <param name="c"></param>
+    /// <param name="c2"></param>
+    /// <param name="speed"></param>
+    /// <returns></returns>
+    public static IEnumerator ScaleUpAndDown(Vector3 oldS, Vector3 newS, RectTransform rt, AnimationCurve c, AnimationCurve c2, float speed1, float speed2)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed1;
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(oldS, newS, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+
+        t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed2;
+            rt.transform.localScale = Utility.UnClampedAnimationLerp(newS, oldS, t, c2);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General Coroutine for moving a Recttransform from one position to new position. with animation curve, and speed.
+    /// </summary>
+    public static IEnumerator MoveToPos(Vector2 oldPos, Vector2 newPos, RectTransform rt, AnimationCurve c, float speed)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            rt.anchoredPosition = UnClampedAnimationLerp(oldPos, newPos, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General Coroutine for moving a Recttransform from one position to new position. with animation curve, with delay.
+    /// </summary>
+    public static IEnumerator MoveToPos(Vector2 oldPos, Vector2 newPos, RectTransform rt, AnimationCurve c, float speed, float delay)
+    {
+
+        if (delay < 0)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed;
+            rt.anchoredPosition = UnClampedAnimationLerp(oldPos, newPos, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General Coroutine for moving a Recttransform from one position to new position. with animation curve, with delay.
+    /// </summary>
+    public static IEnumerator MoveToPos(Vector2 oldPos, Vector2 newPos, RectTransform rt, AnimationCurve c, float speed, float delay, float random)
+    {
+
+        if (delay < 0)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed + Random.Range(-random, random);
+            rt.anchoredPosition = UnClampedAnimationLerp(oldPos, newPos, t, c);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General Coroutine for moving a Recttransform from one position to new position. with animation curve, and speed.
+    /// </summary>
+    public static IEnumerator MoveToPosAndBack(Vector2 oldPos, Vector2 newPos, RectTransform rt, AnimationCurve c1, AnimationCurve c2, float speed1, float speed2)
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed1;
+            rt.anchoredPosition = UnClampedAnimationLerp(oldPos, newPos, t, c1);
+            yield return new WaitForEndOfFrame();
+        }
+
+        t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * speed2;
+            rt.anchoredPosition = UnClampedAnimationLerp(newPos, oldPos, t, c2);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    /// <summary>
+    /// General coroutine for rotating a Recttransform. input 360 for a complete rotation.
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <param name="c"></param>
+    /// <param name="speed"></param>
+    /// <returns></returns>
+    public static IEnumerator Spin(RectTransform rt, AnimationCurve c, float speed, float targetAngle, bool random)
+    {
+        float t = 0;
+        Quaternion startRot = rt.transform.rotation;
+        float orgAngle;
+        Vector3 orgAxis;
+        startRot.ToAngleAxis(out orgAngle, out orgAxis);
+
+        float currentAngle;
+        while (t < 1)
+        {
+            if (random)
+            {
+                t += Time.deltaTime + Random.Range(-0.01f, 0.01f) * speed;
+            }
+            else
+            {
+                t += Time.deltaTime * speed;
+            }
+            
+
+            currentAngle = UnClampedAnimationLerp(orgAngle, targetAngle, t, c);
+            rt.transform.rotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+
+            yield return null;
+        }
+        rt.transform.rotation = Quaternion.AngleAxis(targetAngle, Vector3.forward);
+    }
+
+    /// <summary>
+    /// Rotates briefly in a random direction, then rotates back.
+    /// </summary>
+    /// <returns></returns>
+    public static IEnumerator RotateAskew(RectTransform rt, AnimationCurve c, float speed, float targetAngle)
+    {
+        float t = 0;
+        Quaternion startRot = rt.transform.rotation;
+        float orgAngle;
+        Vector3 orgAxis;
+        startRot.ToAngleAxis(out orgAngle, out orgAxis);
+
+        float currentAngle;
+        while (t < 1)
+        {
+            t += Time.deltaTime * speed;
+
+            currentAngle = Utility.UnClampedAnimationLerp(orgAngle, targetAngle, t, c);
+            rt.transform.rotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+
+            yield return null;
+        }
+
+        t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * (speed/2f);
+
+            currentAngle = Utility.UnClampedAnimationLerp(targetAngle, orgAngle, t, c);
+            rt.transform.rotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+
+            yield return null;
+        }
+
+        rt.transform.rotation = startRot;
+    }
+
+    /// <summary>
+    /// Finds the outer edge position of a BOTTOM rect transform. the position where it is exactly just below the screen. Assumes its anchor/pivot is in the bottom
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <returns></returns>
+    public static Vector2 BottomEdgePosition(RectTransform rt)
+    {
+        return new Vector2(0, -rt.sizeDelta.y);
+    }
+    
+    public static IEnumerator WaitToDisable(GameObject g,float t) {
+        yield return new WaitForSeconds(t);
+        g.SetActive(false);
+    }
+
+
+    public static float Map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+
+
+
+
+
+    /// <summary>
+    /// Created as a precursor to backend integration. When integration is ready, you can exchange these calls with plugin calls.
+    /// </summary>
+    /// <param name="path">The path to the resourceFile you wish to load.</param>
+    /// <returns>Returns a string comprised of the collected data.</returns>
+    public static string LoadDummyText(string path)
+    {
+        return Resources.Load<TextAsset>(path).text;
+    }
+    /// <summary>
+    /// Created as a precursor to backend integration. When integration is ready, you can exchange these calls with plugin calls.
+    /// </summary>
+    /// <param name="path">The path to the resourceFile you wish to load.</param>
+    /// <returns>Returns a sprite created from the collected texture.</returns>
+    public static Sprite LoadDummySprite(string path)
+    {
+        Texture2D tex = Resources.Load<Texture2D>(path);
+        return Sprite.Create(tex, new Rect(0f, 0f, tex.width, tex.height), Vector2.zero);
+    }
+
+
+    public static Vector3 HalfVector()
+    {
+        return Vector3.one * 0.5f;
+    }
+
+}
